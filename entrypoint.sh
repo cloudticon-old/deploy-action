9f20f32630a6dcaa536155fe7d3719e5e2171834
krs <k.kamasa@gmail.com>
2021-07-01 03:23:46 +0200

    inir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
 
-
 mkdir -p functions/node_modules
 mkdir -p ~/.kube
 mkdir -p ~/.docker
 
 cd functions
 
-echo $INPUT_CLOUDTICON_AUTH |  jq -r '.kube' > ~/.kube/config
-echo $INPUT_CLOUDTICON_AUTH |  jq -r '.docker' > ~/.docker/config.json
-cat /github/home/.kube/config
+echo $INPUT_KUBECONFIG |  jq -r '.kube' > ~/.kube/config
+echo $INPUT_CLOUDTICON_AUTH > ~/.docker/config.json
+
+cat /root/.kube/config
+kubectl get pod -A
 cloudticon build
 cloudticon push
 cloudticon helm
